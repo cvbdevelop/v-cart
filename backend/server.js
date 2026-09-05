c18d991f772910687581e90ff982cca52342224b
Chan Vibol <your_email@gmail.com>
2026-09-05 20:20:16 +0700

    បន្ថែម description ទៅក្នុង backend

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ const productSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   image: { type: String, required: true },
   category: { type: String, default: 'general' }, // បន្ថែមជួរនេះ
-  countInStock: { type: Number, required: true, default: 0 } // <-- បន្ថែមថ្មី
+  countInStock: { type: Number, required: true, default: 0 }, // <-- បន្ថែមថ្មី
   description: { type: String }, // <-- បន្ថែមបន្ទាត់នេះចូលទីនេះ
 });
 const Product = mongoose.model('Product', productSchema);
