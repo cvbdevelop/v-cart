58569e156168548835a18abe0dcfdc01c0525aa3
Chan Vibol <your_email@gmail.com>
2026-09-06 11:35:53 +0700

    ជួសជុល syntax និងលំដាប់ tag ក្នុង App.jsx សម្រាប់ OrderTracking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,11 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
 import Admin from './pages/Admin';
+import OrderTracking from './pages/OrderTracking'; // <--- បន្ថែម Import ទំព័រតាមដាន
 import { CartProvider } from './contexts/CartContext';
 import { Toaster } from 'react-hot-toast';
 import ProductDetail from './pages/ProductDetail';
 import Footer from './components/Footer';
-// 1. បន្ថែម Import នៅផ្នែកខាងលើ
-import OrderTracking from './pages/OrderTracking';
 
 function App() {
   return (
@@ -28,8 +27,7 @@ function App() {
               <Route path="/admin" element={<Admin />} />
               <Route path="/login" element={<Login />} />           
               <Route path="/product/:id" element={<ProductDetail />} />
-              // 2. បន្ថែម Route ខាងក្នុង <Routes>
-              <Route path="/tracking" element={<OrderTracking />} />
+              <Route path="/tracking" element={<OrderTracking />} /> {/* <--- Route តាមដាន */}
             </Routes>
           </main>
           
