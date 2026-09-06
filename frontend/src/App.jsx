dc16f1a43ed095e946667207564394ce0c408d99
Chan Vibol <your_email@gmail.com>
2026-09-06 10:39:53 +0700

    ជួសជុលទីតាំង Route Login នៅក្នុង App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,6 @@ import Login from './pages/Login';
 import Admin from './pages/Admin';
 import { CartProvider } from './contexts/CartContext';
 import { Toaster } from 'react-hot-toast';
-
-// ១. Import ឯកសារថ្មីទាំង២
 import ProductDetail from './pages/ProductDetail';
 import Footer from './components/Footer';
 
@@ -16,27 +14,21 @@ function App() {
   return (
     <CartProvider>
       <Router>
-        {/* បន្ថែម flex និង flex-col ដើម្បីរុញ Footer ទៅបាតក្រោមជានិច្ច */}
         <div className="min-h-screen flex flex-col">
           <Navbar />
           <Toaster position="top-center" reverseOrder={false} />
           
-          {/* រុំ Routes ជាមួយ main និង flex-grow */}
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/checkout" element={<Checkout />} />
-              // បន្ថែម Route ថ្មីនៅក្រោម <Route path="/checkout" ... />
-              <Route path="/login" element={<Login />} />
               <Route path="/admin" element={<Admin />} />
-              
-              {/* ២. បន្ថែម Route សម្រាប់ទំព័រលម្អិតទំនិញ */}
+              <Route path="/login" element={<Login />} />           
               <Route path="/product/:id" element={<ProductDetail />} />
             </Routes>
           </main>
           
-          {/* ៣. ដាក់ Footer នៅខាងក្រោមគេបង្អស់ */}
           <Footer />
         </div>
       </Router>
