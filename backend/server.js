a2a50e02303539aad11192fa8391eada0f01a8ac
Chan Vibol <your_email@gmail.com>
2026-09-10 18:35:03 +0700

    បន្ថែម cors ចូលវិញដើម្បីដោះស្រាយបញ្ហា Login Error

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ if (!fs.existsSync(path.join(__dirname, 'uploads'))) {
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
